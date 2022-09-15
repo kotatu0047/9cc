0eb56d5d2eec662b86115d8b8fb7e94daf02536c
kotatu <yamamichi1995@outlook.jp>
2022-09-16 08:52:19 +0900

    足し算と引き算を追加

--- 9cc-lib/9cc.c
+++ 9cc-lib/9cc.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     fprintf(stderr, "引数の個数が正しくありません\n");
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+  while (*p)
+  {
+    if (*p == '+')
+    {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-')
+    {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+  }
+
   printf("  ret\n");
   return 0;
 }
